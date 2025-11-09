d1abcf9fdf91eb933f8c0c0f390d6595b4784b81
Sterling Smith <aguynamedsterl@gmail.com>
2025-11-09 15:50:22 -0800

    updated params

--- VCU/firmware/app/generated/pedalMapGenerated.hpp
+++ VCU/firmware/app/generated/pedalMapGenerated.hpp
@@ -13,20 +13,20 @@ namespace PedalMap
 {
 
 // Taylor series for power function
-// Exponent p = 1.3
+// Exponent p = 1.5
 // Order N = 6
-// Point of approximation a = 0.5
+// Point of approximation a = 0.75
 
-static constexpr float PEDAL_MAP_POINT_A { 0.5F };
+static constexpr float PEDAL_MAP_POINT_A { 0.75F };
 static constexpr size_t PEDAL_MAP_ODER_N { 6U };
 static constexpr float PEDAL_MAP_COEFFS[PEDAL_MAP_ODER_N + 1U] = {
-    0.40612619817811774F,
-    1.0559281152631061F,
-    0.3167784345789319F,
-    -0.14782993613683487F,
-    0.12565544571630965F,
-    -0.13570788137361445F,
-    0.16737305369412447F,
+    0.649519052838329F,
+    1.299038105676658F,
+    0.4330127018922193F,
+    -0.09622504486493762F,
+    0.04811252243246881F,
+    -0.032075014954979206F,
+    0.02494723385387272F,
 };
 
 /***************************************************************************************************
